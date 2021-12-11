08dda0a32f19bd121ceedbf495491c5d6838f704
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-12-11 21:23:55 +0100

    build: add standard-version

---
+++ .versionrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bumpFiles: [
+    {
+      filename: "version.txt",
+      type: "plain-text",
+    },
+    {
+      filename: "package.json",
+      type: "json",
+    },
+  ],
+};
