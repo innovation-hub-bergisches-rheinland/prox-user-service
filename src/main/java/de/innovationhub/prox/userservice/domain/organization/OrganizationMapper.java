b31ff5365b505eab1cccdd3c4ea588d01422d447
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-12-28 13:25:44 +0100

    chore: add Domain Mapper for Post DTO

--- src/main/java/de/innovationhub/prox/userservice/domain/organization/OrganizationMapper.java
+++ src/main/java/de/innovationhub/prox/userservice/domain/organization/OrganizationMapper.java
@@ -1,7 +1,10 @@
 package de.innovationhub.prox.userservice.domain.organization;
 
 import de.innovationhub.prox.userservice.domain.organization.dto.OrganizationGetDto;
+import de.innovationhub.prox.userservice.domain.organization.dto.OrganizationPostDto;
+import de.innovationhub.prox.userservice.domain.user.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "cdi")
@@ -9,4 +12,8 @@ public interface OrganizationMapper {
   OrganizationMapper INSTANCE = Mappers.getMapper(OrganizationMapper.class);
 
   OrganizationGetDto toGetDto(Organization organization);
+
+  @Mapping(source = "dto.name", target = "name")
+  @Mapping(source = "user", target = "owner")
+  Organization toDomainObject(OrganizationPostDto dto, User owner);
 }
--- src/test/java/de/innovationhub/prox/userservice/domain/organization/OrganizationMapperTest.java
+++ src/test/java/de/innovationhub/prox/userservice/domain/organization/OrganizationMapperTest.java
@@ -2,7 +2,11 @@ package de.innovationhub.prox.userservice.domain.organization;
 
 import static org.assertj.core.api.Assertions.*;
 
+import de.innovationhub.prox.userservice.domain.organization.dto.OrganizationPostDto;
 import de.innovationhub.prox.userservice.domain.user.User;
+import de.innovationhub.prox.userservice.domain.user.UserJpa;
+import de.innovationhub.prox.userservice.domain.user.UserMapper;
+import java.util.Set;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 
@@ -22,4 +26,36 @@ class OrganizationMapperTest {
     assertThat(organizationDto.id()).isNotNull();
     assertThat(organizationDto.name()).isEqualTo("Musterfirma GmbH & Co. KG");
   }
+
+  @Test
+  void shouldMapPostDtoToOrganization() {
+    // Given
+    var userId = UUID.randomUUID();
+    var user = new User(userId);
+    var organizationPostDto = new OrganizationPostDto("Musterfirma GmbH & Co. KG");
+
+    // When
+    var organization = OrganizationMapper.INSTANCE.toDomainObject(organizationPostDto, user);
+
+    // Then
+    assertThat(organization).isNotNull();
+    assertThat(organization.getId()).isNotNull();
+    assertThat(organization.getName()).isEqualTo("Musterfirma GmbH & Co. KG");
+    assertThat(organization.getMembers()).hasSize(1);
+    assertThat(organization.getMembers().get(user)).isNotNull();
+    assertThat(organization.getMembers().get(user).role()).isEqualTo(OrganizationRole.OWNER);
+  }
+
+  /*@Test
+  void shouldMapOrganizationJpaToOrganization() {
+    // Given
+    Organization.builder().m
+
+    // When
+    var user = UserMapper.INSTANCE.toDomain(userJpa);
+
+    // Then
+    assertThat(user).isNotNull();
+    assertThat(user.getId()).isEqualTo(id);
+  }*/
 }
