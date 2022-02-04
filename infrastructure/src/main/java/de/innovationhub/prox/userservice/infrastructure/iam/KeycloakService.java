c871797436820f636dd327835570a20ecb338a05
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-02-04 17:04:46 +0100

    perf: introduce caching

--- infrastructure/src/main/java/de/innovationhub/prox/userservice/infrastructure/iam/KeycloakService.java
+++ infrastructure/src/main/java/de/innovationhub/prox/userservice/infrastructure/iam/KeycloakService.java
@@ -1,5 +1,6 @@
 package de.innovationhub.prox.userservice.infrastructure.iam;
 
+import io.quarkus.cache.CacheResult;
 import java.util.Optional;
 import java.util.UUID;
 import javax.enterprise.context.ApplicationScoped;
@@ -21,21 +22,23 @@ public class KeycloakService {
     this.usersResource = realmResource.users();
   }
 
+  @CacheResult(cacheName = "keycloak-user-cache")
   public Optional<UserRepresentation> findById(UUID id) {
     try {
       var result = this.usersResource.get(id.toString()).toRepresentation();
       return Optional.of(result);
     } catch (WebApplicationException e) {
-      // If we receive a 404 status, the user doesn't exist and we can return an empty result
+      // If we receive a 404 status, the user doesn't exist, and we can return an empty result
       if(e.getResponse().getStatus() == 404) {
         return Optional.empty();
       }
 
-      // Otherwise the response is errornous and therefore we need to rethrow the exception
+      // Otherwise, the response is erroneous, and therefore we need to rethrow the exception
       throw e;
     }
   }
 
+  @CacheResult(cacheName = "keycloak-users-search-cache")
   public Iterable<UserRepresentation> search(String query) {
     return this.realmResource.users().search(query, 0, 100, true);
   }
