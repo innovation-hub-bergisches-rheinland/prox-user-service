5d4fe79c4f5d528e8a2a1d426f7c8739d826b678
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-02-06 18:13:51 +0100

    build: add helm charts

--- .versionrc.js
+++ .versionrc.js
@@ -5,6 +5,11 @@ module.exports = {
       type: "plain-text",
     },
     {
+      filename: "./deploy/charts/prox-user-service/Chart.yaml",
+      updater:
+          "./node_modules/@map-colonies/standard-version-update-helm-version/src/index.js",
+    },
+    {
       filename: "package.json",
       type: "json",
     },
