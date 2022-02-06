74bac311daa809c8565b12f01490ef9049d95aea
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-02-06 02:35:01 +0100

    chore: remove lint staged and use a "plain" pre-commit hook instead

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Delegate the whole formatting to spotless
-  "*": [
-    (resolvedPaths) =>
-      resolvedPaths.map(
-        (filename) => `./gradlew spotlessApply -PspotlessIdeHook="${filename}"`
-      ),
-  ],
-};
