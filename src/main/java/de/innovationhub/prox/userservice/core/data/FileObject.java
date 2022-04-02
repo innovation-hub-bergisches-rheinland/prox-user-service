bbe3b014e94eee1bed4242f9968f0145295f3b87
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-04-02 16:22:04 +0200

    refactor: move response building logic to file object

--- src/main/java/de/innovationhub/prox/userservice/core/data/FileObject.java
+++ src/main/java/de/innovationhub/prox/userservice/core/data/FileObject.java
@@ -1,5 +1,8 @@
 package de.innovationhub.prox.userservice.core.data;
 
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import lombok.Value;
 
 @Value
@@ -7,4 +10,16 @@ public class FileObject {
   private String key;
   private String mimeType;
   private byte[] data;
+
+  public Response createResponse() {
+    ResponseBuilder response = Response.ok(this.getData());
+    response.header("Content-Disposition", "attachment;filename=" + this.getKey());
+    response.header("Content-Type", this.getMimeType());
+
+    if (!this.getMimeType().equalsIgnoreCase("image/png")
+        && !this.getMimeType().equalsIgnoreCase("image/jpeg"))
+      throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
+
+    return response.build();
+  }
 }
