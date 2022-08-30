a172ecaf5191e3d2d6cdf0052ea03c6b47effbdd
Tobias Hund <tobias.hund@innovation-hub.de>
2022-08-30 10:16:27 +0200

    feat: add s3client method to get object url

--- src/main/java/de/innovationhub/prox/userservice/core/data/S3ObjectStore.java
+++ src/main/java/de/innovationhub/prox/userservice/core/data/S3ObjectStore.java
@@ -1,11 +1,14 @@
 package de.innovationhub.prox.userservice.core.data;
 
+import io.quarkus.cache.CacheResult;
 import java.io.IOException;
+import java.net.URL;
 import javax.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
 import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
@@ -26,6 +29,18 @@ public class S3ObjectStore implements ObjectStoreRepository {
     try {
       var obj = s3Client.getObject(request);
       return new FileObject(key, obj.response().contentType(), obj.readAllBytes());
+    } catch (NoSuchKeyException e) {
+      throw new ObjectNotFoundException();
+    }
+  }
+
+  @Override
+  @CacheResult(cacheName = "s3-object-url")
+  public URL getObjectUrl(String key) {
+    var request = GetUrlRequest.builder().bucket(bucket).key(key).build();
+
+    try {
+      return s3Client.utilities().getUrl(request);
     } catch (NoSuchKeyException e) {
       throw new ObjectNotFoundException();
     }
